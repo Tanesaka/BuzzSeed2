87d275f845c233541d16cdbb060d821066664ded
Tanesaka <seedtanesa@outlook.jp>
2021-01-10 09:40:48 +0000

    Update

--- app/controllers/public/relations_controller.rb
+++ app/controllers/public/relations_controller.rb
@@ -4,22 +4,20 @@ class Public::RelationsController < ApplicationController
   def create
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
+      redirect_to request.referer
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
+      redirect_to request.referer
     end
   end
 
   def destroy
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
+      redirect_to request.referer
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
+      redirect_to request.referer
     end
   end
 
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -24,6 +24,7 @@ class Public::UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
 
   def following
