2907078f16144f234fa5988bf1e5635f7d7d44c8
Tanesaka <seedtanesa@outlook.jp>
2021-01-12 17:00:56 +0000

    toppage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,12 @@
 class HomesController < ApplicationController
   def top
+    @answers = Answer.all
+    # randomに取得し、idに代入、最後に一倍最初のidを1つ取得
+    @answer = Answer.where( 'id >= ?', rand(Answer.first.id..Answer.last.id) ).first
+    @favorites = Favorite.all
+    @themes = Theme.all
   end
-  
+
   def about
   end
 end
---
+++ db/migrate/20210112151257_drop_review.rb
@@ -0,0 +1,5 @@
+class DropReview < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :reviews
+  end
+end
