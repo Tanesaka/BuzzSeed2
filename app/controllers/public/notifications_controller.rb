fcf2ad5daa7bdddc3eacb7b1d4ac55151f15a623
Tanesaka <seedtanesa@outlook.jp>
2021-01-13 18:19:58 +0000

    Update

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,6 @@
 class Public::NotificationsController < ApplicationController
   def index
-      @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+      @notifications = current_user.passive_notifications.page(params[:page]).per(10)
       @notifications.where(checked: false).each do |notification|
         notification.update_attributes(checked: true)
     end
