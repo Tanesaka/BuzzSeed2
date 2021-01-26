878435e4e2a49b2e1bce087d3077bd736abb01b0
Tanesaka <seedtanesa@outlook.jp>
2021-01-26 11:40:49 +0000

    Update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   before_action :check_guest, only: [:edit]
-  before_action :twitter_client, only: [:tweet]
 
   def check_guest
     if current_user.email == 'guest@example.com'
@@ -38,28 +37,7 @@ class Public::UsersController < ApplicationController
     end
     @tweet = Tweet.new
   end
-
-  def tweet
-    @user = current_user
-    @answers = @user.answers
-    @favorites_count = 0
-    @answers.each do |answer|
-      @favorites_count += answer.favorites.count
-    end
-    @tweet = @user.tweets.new(tweet_params)
-    if @tweet.save
-      @client.update!(@user.name + "はBuzzSeedを利用しています。\r
-      総ヒトコト数：" + @answers.count.to_s + "\r
-      総獲得Good!数：" + @favorites_count.to_s + "
-      \r" + @user.name + "のヒトコトを見る\r
-      buzzseed.info/users/" + @user.id.to_s + "
-      \r" + @tweet.content + "\r#BuzzSeed")
-      redirect_to user_path(current_user), notice: 'tweetが完了しました！'
-    else
-      redirect_to user_path(current_user), alert: 'tweetできませんでした…（追加コメントは60字までです。）'
-    end
-  end
-
+  
   def index
     # ユーザー検索フォームに受け渡す変数
     @q = User.ransack(params[:q])
@@ -88,16 +66,4 @@ class Public::UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :unique_code)
   end
 
-  def twitter_client
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['api_key']
-      config.consumer_secret = ENV['api_secret_key']
-      config.access_token = ENV['access_token']
-      config.access_token_secret = ENV['access_token_secret']
-    end
-  end
-
-  def tweet_params
-    params.require(:tweet).permit(:content)
-  end
 end
