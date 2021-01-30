8f1d0b7f1cd3bc48e8e99ee4ccddb15b5d368411
Tanesaka <seedtanesa@outlook.jp>
2021-01-30 16:05:58 +0000

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_083939) do
+ActiveRecord::Schema.define(version: 2021_01_30_152359) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_083939) do
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
     t.boolean "is_active", default: true, null: false
+    t.boolean "beginner", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
