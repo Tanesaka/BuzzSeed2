c0438a57aa78b730fbaf7e651787f9f0a0ac0980
Tanesaka <seedtanesa@outlook.jp>
2021-02-16 06:24:02 +0000

    Update

--- app/controllers/public/themes_controller.rb
+++ app/controllers/public/themes_controller.rb
@@ -1,5 +1,5 @@
 class Public::ThemesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :rankindex, :show]
   def new
     @theme = Theme.new
   end
